cc01d143048955a1bb22d0a36a8a361b7f1bec2d
TheInfoGuru <scates09@gmail.com>
2023-11-22 04:02:07 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from inventory import MENU
+
+resources = {  # default global variable
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
+
+# TODO Prompt user until user gives valid answer
+def prompt_customer():
+    input("What would you like? (espresso/latte/cappuccino): ")
+
+# TODO Turn off coffee machine
+
+# TODO Print report
+
+# TODO check for sufficient resources
+
+# TODO process coins
+
+# TODO verify user gave enough money, calculate change and increase revenue
+
+# TODO make the coffee, and accurate reduce inventory
+
+# TODO Tell user to enjoy drink
+
+# Test Section
+prompt_customer()
+
+# True Section
+
+while True:
+    pass
+
