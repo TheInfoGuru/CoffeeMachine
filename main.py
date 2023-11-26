317391d10dd2abce1595e41d8e9438d3e22f8b5e
The Info Guru <scates09@gmail.com>
2023-11-26 15:58:22 -0600

    Changes made on laptop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from inventory import MENU
+from artwork import logo
 
 resources = {  # default global variable
     "water": 300,
@@ -9,13 +10,35 @@ resources = {  # default global variable
 
 # TODO Prompt user until user gives valid answer
 def prompt_customer():
-    input("What would you like? (espresso/latte/cappuccino): ")
+    answer = ""
+    while answer not in MENU:
+        if answer != "":
+            print("You chose an incorrect option. Please try again.")
+        answer = input("What would you like? (espresso/latte/cappuccino): ")
+    return answer
 
 # TODO Turn off coffee machine
+def turn_off():
+    exit()
+
+def get_drink(response: str):
+    if response == "off":
+        turn_off()
+    elif response == "report":
+        run_report()
+    return MENU[response]
 
 # TODO Print report
+def run_report():
+    pass
 
 # TODO check for sufficient resources
+def sufficient_inventory(ingredients: dict):
+    for ingredient in ingredients:
+        print(f"drink ingredient: {ingredients[ingredient]}, resources ingredient: {resources[ingredient]}")
+        if resources[ingredient] < ingredients[ingredient]:
+
+
 
 # TODO process coins
 
@@ -25,11 +48,16 @@ def prompt_customer():
 
 # TODO Tell user to enjoy drink
 
-# Test Section
-prompt_customer()
+print(logo)
 
-# True Section
+drink_choice = prompt_customer()
+drink_details = get_drink(drink_choice)
+print(f"drink details: {drink_details}")
+print(drink_details["ingredients"])
+sufficient_inventory(drink_details["ingredients"])
 
-while True:
-    pass
 
+
+
+#while True:
+    #pass
